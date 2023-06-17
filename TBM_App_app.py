ba2e5e37023adfbad57cc75fc6ddbbf6933dde34
Kursat Kilic <kilic_kursat@hotmail.com>
2023-06-17 21:40:55 +0900

    Update TBM_App_app.py

--- TBM_App_app.py
+++ TBM_App_app.py
@@ -62,8 +62,64 @@ def run():
     #st.sidebar.image(image_hospital)
 
     #st.title("Insurance Charges Prediction App")
-    
+def user_input_features():
     if add_selectbox == 'Batch':
+        pressure_gauge1 = st.sidebar.number_input('Pressure gauge 1 (kPa)', min_value=float(df['pressure_gauge1'].min()), value=0)
+        pressure_gauge2 = st.sidebar.number_input('Pressure gauge 2 (kPa)', min_value=float(df['pressure_gauge2'].min()), value=0)
+        pressure_gauge3 = st.sidebar.number_input('Pressure gauge 3 (kPa)', min_value=float(df['pressure_gauge3'].min()), value=0)
+        pressure_gauge4 = st.sidebar.number_input('Pressure gauge 4 (kPa)', min_value=float(df['pressure_gauge4'].min()), value=0)
+        digging_velocity_left = st.sidebar.number_input('Digging velocity left (mm/min)', min_value=float(df['digging_velocity_left'].min()), value=0)
+        digging_velocity_right = st.sidebar.number_input('Digging velocity right (mm/min)', min_value=float(df['digging_velocity_right'].min()), value=0)
+        shield_jack_stroke_left = st.sidebar.number_input('Shield jack stroke left (mm)', min_value=float(df['shield_jack_stroke_left'].min()), value=0)
+        shield_jack_stroke_right = st.sidebar.number_input('Shield jack stroke right (mm)', min_value=float(df['shield_jack_stroke_right'].min()), value=0)
+        propulsion_pressure = st.sidebar.number_input('Propulsion pressure (MPa)', min_value=float(df['propulsion_pressure'].min()), value=0)
+        total_thrust = st.sidebar.number_input('Total thrust (kN)', min_value=float(df['total_thrust'].min()), value=0)
+        cutter_torque = st.sidebar.number_input('Cutter torque (kNm)', min_value=float(df['cutter_torque'].min()), value=0)
+        cutterhead_rotation_speed = st.sidebar.number_input('Cutterhead rotation speed (rpm)', min_value=float(df['cutterhead_rotation_speed'].min()), value=0)
+        screw_pressure = st.sidebar.number_input('Screw pressure (MPa)', min_value=float(df['screw_pressure'].min()), value=0)
+        screw_rotation_speed = st.sidebar.number_input('Screw rotation speed (rpm)', min_value=float(df['screw_rotation_speed'].min()), value=0)
+        gate_opening = st.sidebar.number_input('Gate opening (%)', min_value=float(df['gate_opening'].min()), max_value=100, value=0)
+        mud_injection_pressure = st.sidebar.number_input('Mud injection pressure (MPa)', min_value=float(df['mud_injection_pressure'].min()), value=0)
+        add_mud_flow = st.sidebar.number_input('Add mud flow (L/min)', min_value=float(df['add_mud_flow'].min()), value=0)
+        back_in_injection_rate = st.sidebar.number_input('Back in injection rate (%)', min_value=float(df['back_in_injection_rate'].min()), max_value=100, value=0)
+        output = ""
+
+        output_dict = {'VCS': 0, 'VG': 1,'VSG': 2 }
+        output_df = pd.DataFrame([output_dict])
+        
+         data = {
+        'pressure_gauge1': pressure_gauge1,
+        'pressure_gauge2': pressure_gauge2,
+        'pressure_gauge3': pressure_gauge3,
+        'pressure_gauge4': pressure_gauge4,
+        'digging_velocity_left': digging_velocity_left,
+        'digging_velocity_right': digging_velocity_right,
+        'shield_jack_stroke_left': shield_jack_stroke_left,
+        'shield_jack_stroke_right': shield_jack_stroke_right,
+        'propulsion_pressure': propulsion_pressure,
+        'total_thrust': total_thrust,
+        'cutter_torque': cutter_torque,
+        'cutterhead_rotation_speed': cutterhead_rotation_speed,
+        'screw_pressure': screw_pressure,
+        'screw_rotation_speed': screw_rotation_speed,
+        'gate_opening': gate_opening,
+        'mud_injection_pressure': mud_injection_pressure,
+        'add_mud_flow': add_mud_flow,
+        'back_in_injection_rate': back_in_injection_rate,
+         'VCS': 0, 
+         'VG': 1,
+         'VSG': 2
+    }
+
+    features = pd.DataFrame(data, index=[0])
+    return features
+
+df = user_input_features()
+
+
+    # Rest of the code
+
+    """
         pressure_gauge1 = st.number_input('Pressure gauge 1 (kPa)', min_value=0, value=0)
         pressure_gauge2 = st.number_input('Pressure gauge 2 (kPa)', min_value=0, value=0)
         pressure_gauge3 = st.number_input('Pressure gauge 3 (kPa)', min_value=0, value=0)
@@ -87,7 +143,7 @@ def run():
 
         output_dict = {'VCS': 0, 'VG': 1,'VSG': 2 }
         output_df = pd.DataFrame([output_dict])
-        
+"""        
     if st.button("Train Model"):
             # Prediction on unseen data
             unseen_data = predict_model(model, data=df2)
